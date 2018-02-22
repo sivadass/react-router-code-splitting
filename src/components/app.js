b462f068f74640662bc6d22e1e9068b1a5c95870
Sivadass N <nsivadass@gmail.com>
2018-02-22 23:57:44 +0530

    fixed image url

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
-import loaderIcon from './imges/logo.png'
+import loaderIcon from './imges/loading.gif'
 
 class DynamicImport extends React.Component {
   constructor(props){
